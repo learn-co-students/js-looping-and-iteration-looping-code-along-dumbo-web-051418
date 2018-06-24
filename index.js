b5a9582a8c67e5ba5d0d61d01828968b10f22fd5
pablopawpaw <lpault29@gmail.com>
2018-06-24 18:05:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(arrayOfEmployees) {
+  for(let i = 0; i < arrayOfEmployees.length; i++) {
+    console.log(`Welcome ${arrayOfEmployees[i]}! You are employee #${i+1}.`)
+  };
+  return arrayOfEmployees;
+};
+
+function tailsNeverFails() {
+  function coinToss() {
+    return Math.random() >= 0.5;
+  };
+  let consecutiveTails = 0;
+  while(coinToss()) {
+    consecutiveTails++
+  }
+  return `You got ${consecutiveTails} tails in a row!`
+};
