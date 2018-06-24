e9fc9b8f0e87f0e393c6406ea52bbb75a801bf79
prmoreira23 <prm.gredes@gmail.com>
2018-06-24 00:48:03 -0400

    Add functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(arr){
+  let counter = 1;
+  arr.forEach(function(element){
+    console.log("Welcome "+element+"! You are employee #"+(counter++)+".");
+  });
+  return arr;
+}
+
+function tailsNeverFails(){
+  let counter = 0;
+  while(Math.random() >= 0.5){
+    counter++;
+  }
+  return "You got "+counter+" tails in a row!";
+}
